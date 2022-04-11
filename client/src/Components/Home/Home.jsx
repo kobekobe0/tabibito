2e0ee59f205c9b744c880781dcf95a97c1e820c3
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-04-12 00:57:45 +0800

    auth(on progress)

---
+++ client/src/Components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { Jwt } from 'jsonwebtoken'
+function Home() {
+    //function for fetching data from the server
+
+    useEffect(() => {
+        const token = window.localStorage.getItem('user')
+        if (token) {
+            if (!user) {
+                window.localStorage.removeItem('user')
+                window.location.href = '/login'
+            } else {
+                //fetch data
+            }
+        }
+    }, [])
+    return <div>Home</div>
+}
+
+export default Home
