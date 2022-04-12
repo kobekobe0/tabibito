51670e47f6c115c167ce801318ac9c85c269f97e
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-04-12 21:48:21 +0800

    auth done

--- server/controllers/register/register.controller.js
+++ server/controllers/register/register.controller.js
@@ -7,7 +7,7 @@ const registerUser = async (req, res) => {
         const { name, email, password } = req.body
         const user = { name, email, password }
         await User.create(user)
-        res.send(user)
+        res.json(user)
     } catch (error) {
         console.log(error)
         res.status(500).json({
@@ -18,7 +18,10 @@ const registerUser = async (req, res) => {
 }
 
 const loginUser = async (req, res) => {
-    const user = await User.findOne({ email: req.body.email })
+    const user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password,
+    })
 
     if (user) {
         const token = jwt.sign(
@@ -33,4 +36,19 @@ const loginUser = async (req, res) => {
     return res.json({ user: false })
 }
 
-module.exports = { registerUser, loginUser }
+const verifyLogin = async (req, res) => {
+    const token = req.headers['token']
+
+    try {
+        const decoded = jwt.verify(token, 'secretkey')
+        const email = decoded.email
+        const user = await User.findOne({ email: email })\
+
+        return res.json
+    } catch (error) {
+        console.log(error)
+        return res.json({ status: 'error' })
+    }
+}
+
+module.exports = { registerUser, loginUser, verifyLogin }
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const {
     registerUser,
     loginUser,
+    verifyLogin,
 } = require('./controllers/register/register.controller')
 mongoose.connect('mongodb://localhost:27017/tabibito')
 
@@ -14,6 +15,7 @@ app.use(cors())
 app.use(express.json())
 app.post('/api/users/register', registerUser)
 app.post('/api/users/login', loginUser)
+app.get('/api/users/login', verifyLogin)
 app.listen(PORT || 3000, () => {
     console.log('Server is running on port ' + PORT)
 })
