2bd3ac30ca0a8ac8c42c34232a798e4398f92388
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-04-14 22:50:39 +0800

    auth middleware

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const PORT = 3000
 
 const Travel = require('./models/travel.model')
+const { reqAuth } = require('./middleware/auth/auth')
 
 const mongoose = require('mongoose')
 const {
@@ -23,6 +24,7 @@ mongoose.connect('mongodb://localhost:27017/tabibito')
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/travel', reqAuth)
 app.post('/api/users/register', registerUser)
 app.post('/api/users/login', loginUser)
 app.get('/api/users/login', verifyLogin)
---
+++ server/middleware/auth/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+const User = require('../../models/register.model')
+
+const reqAuth = async (req, res, next) => {
+    console.log('reqAuth')
+    const token = req.headers.authorization
+
+    jwt.verify(
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoia29iZSIsImVtYWlsIjoia29iZUBrb2JlIiwiaWF0IjoxNjQ5ODU3MjgyfQ.9HQUzTfPiGhY9Ws3Hjm9bi3DxmD0D-VtD46rP9NedK8',
+        'secretkey', //just a placeholder, send a real headers from FE
+        async function (err, decoded) {
+            console.log(decoded) // bar
+            if (decoded.email) {
+                const user = await User.findOne({ email: decoded.email })
+                if (user !== null) {
+                    return res.json({
+                        status: 'error',
+                        message: 'User not found',
+                    })
+                }
+                return next()
+            }
+        }
+    )
+}
+
+module.exports = { reqAuth }
