6172583eec3d0ac542152ab17c1787182154ee75
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-04-15 00:05:25 +0800

    register/login figma

--- client/src/Components/Register/Register.jsx
+++ client/src/Components/Register/Register.jsx
@@ -2,13 +2,25 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import jwt_decode from 'jwt-decode'
+import './register.css'
 
 function Register() {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [consfirmPassword, setConfirmPassword] = useState('')
+
     const handleSubmit = async (e) => {
         e.preventDefault()
+
+        if (!name) return alert('Name is required')
+        if (!email) return alert('Email is required')
+        if (!password) return alert('Password is required')
+
+        if (password !== consfirmPassword) {
+            alert('Passwords do not match')
+            return
+        }
 
         const res = await axios.post(
             'http://localhost:3000/api/users/register',
@@ -33,38 +45,48 @@ function Register() {
             console.log(user)
             if (user) {
                 window.location.href = '/'
-            } else {
-                //fetch data
             }
         }
     }, [])
+
     return (
-        <div>
-            <h1>Register</h1>
-            <div>
-                <form>
-                    <input
-                        type="text"
-                        placeholder="enter your name"
-                        onChange={(e) => setName(e.target.value)}
-                    />{' '}
-                    <br />
-                    <input
-                        type="text"
-                        placeholder="enter your email"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />{' '}
-                    <br />
-                    <input
-                        type="password"
-                        placeholder="enter your password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <br />
-                    <button type="submit" onClick={handleSubmit}>
-                        Register
-                    </button>
-                </form>
+        <div className="register_page">
+            <div className="register_form">
+                <h1>Register</h1>
+                <div className="register_fields">
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="enter your name"
+                            onChange={(e) => setName(e.target.value)}
+                            maxLength="15"
+                        />{' '}
+                        <br />
+                        <input
+                            type="text"
+                            placeholder="enter your email"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />{' '}
+                        <br />
+                        <input
+                            type="password"
+                            placeholder="enter your password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <br />
+                        <input
+                            type="password"
+                            placeholder="enter your password"
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                        <button type="submit" onClick={handleSubmit}>
+                            Register
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div className="register_image">
+                <img src="../../images/register.jpg" />
             </div>
         </div>
     )
