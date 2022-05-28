7351a5513e3f0f62a3782022036bfd602225c052
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-05-28 21:58:34 +0800

    carousel

--- client/src/Components/TravelPage/TravelPage.jsx
+++ client/src/Components/TravelPage/TravelPage.jsx
@@ -6,12 +6,14 @@ function TravelPage() {
     const { id } = useParams()
     const [data, setData] = useState({})
     const [image, setImage] = useState([])
-    const [counter, setCounter] = useState(1)
-    const [imageToShow, setImageToShow] = useState(image[0])
+    const [counter, setCounter] = useState(0)
+
     useEffect(() => {
+        setData({})
         axios
             .get(`http://localhost:3000/api/travel/${id}`)
             .then((res) => {
+                console.log(res.data)
                 setData(res.data)
             })
             .catch((err) => {
@@ -19,6 +21,7 @@ function TravelPage() {
             })
     }, [])
     useEffect(() => {
+        setImage([])
         if (data.images) {
             data.images.forEach((image) => {
                 const imagePath = image.replace('uploads', '')
@@ -28,20 +31,21 @@ function TravelPage() {
         }
     }, [data])
 
-    useEffect(() => {
-        setImageToShow(image[0])
-    }, [image])
+    const next = async (i) => {
+        if (counter < image.length - 1) {
+            setCounter(counter + 1)
+        }
 
-    const next = async () => {
-        setCounter(counter + 1)
-        setImageToShow(image[counter])
-        console.log(counter)
+        console.log(counter + 1)
+        console.log(typeof image.length)
     }
 
-    const prev = () => {
-        setCounter(counter - 1)
-        setImageToShow(image[counter])
-        console.log(image[counter])
+    const prev = (i) => {
+        if (counter > 0) {
+            setCounter(counter - 1)
+        }
+
+        console.log(counter)
     }
     return (
         <>
@@ -49,18 +53,54 @@ function TravelPage() {
                 <div className="main">
                     <section className="image-holder">
                         <div className="image-slider">
-                            {counter < image.length ? (
-                                <div onClick={next}>next</div>
-                            ) : null}
-
-                            {counter == image.length || counter !== 1 ? (
-                                <div onClick={prev}>prev</div>
-                            ) : null}
-                            <img
-                                src={`http://localhost:3000/${imageToShow}`}
-                                alt=""
-                                style={{ width: '300px' }}
-                            />
+                            {image
+                                ? image.map((image, index) => (
+                                      <>
+                                          <img
+                                              src={`http://localhost:3000/${image}`}
+                                              alt="travel"
+                                              style={{
+                                                  display:
+                                                      counter === index
+                                                          ? 'block'
+                                                          : 'none',
+                                                  opacity:
+                                                      counter === index
+                                                          ? '1'
+                                                          : '0',
+                                              }}
+                                              id={`image${index}`}
+                                              className="image"
+                                          />
+                                          {image.length > counter && (
+                                              <div
+                                                  id="next"
+                                                  onClick={() => next(index)}
+                                                  style={{
+                                                      display:
+                                                          image.length !==
+                                                          counter + 1
+                                                              ? null
+                                                              : 'none',
+                                                  }}
+                                              >
+                                                  next
+                                              </div>
+                                          )}
+                                          {counter > 0 ? (
+                                              <div
+                                                  id="prev"
+                                                  onClick={() => prev(index)}
+                                              >
+                                                  prev
+                                              </div>
+                                          ) : null}
+                                      </>
+                                  ))
+                                : null}
+                        </div>
+                        <div>
+                            <h2>{data.title}</h2>
                         </div>
                     </section>
                     <section className="post-description"></section>
