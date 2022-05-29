5c8996009eb743c13abfbe2acbabaf1f1e4f6464
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-05-29 14:12:09 +0800

    user model pfp and background notes

--- server/models/register.model.js
+++ server/models/register.model.js
@@ -5,6 +5,8 @@ const User = new mongoose.Schema(
         name: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
+        //add user pfp later
+        //add user background later
     },
     { collection: 'user-data' }
 )
