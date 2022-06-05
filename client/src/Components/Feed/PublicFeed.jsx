c42fa3940977e1e09a97157481ad711c2d597ee6
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-06-06 00:11:50 +0800

    feed header

--- client/src/Components/Feed/PublicFeed.jsx
+++ client/src/Components/Feed/PublicFeed.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react'
+import './PublicFeed.css'
+import jwt_decode from 'jwt-decode'
+
 function PublicFeed() {
+    const [profileImg, setProfileImg] = useState('')
+    useEffect(() => {
+        //get token from localStorage
+        const token = window.localStorage.getItem('user')
+        if (token) {
+            //decode token
+            let userData = jwt_decode(token)
+            console.log(userData)
+            setProfileImg(userData.pfp.replace('pfp', ''))
+        }
+    })
     return (
         <>
             {' '}
             <div className="public-feed">
-                <main></main>
+                <main>
+                    <div className="feed-header">
+                        <h1>EXPLORE 🧭</h1>
+                        <img
+                            src={`http://localhost:3000/${profileImg}`}
+                            alt=""
+                        />
+                    </div>
+                </main>
             </div>
         </>
     )
