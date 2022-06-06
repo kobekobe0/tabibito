ab5d6bd60afad0ceaecb242c18bf2cbe2455889f
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-06-06 21:44:07 +0800

    feed card - no animation

--- client/src/Components/Feed/PublicFeed.jsx
+++ client/src/Components/Feed/PublicFeed.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import './PublicFeed.css'
 import jwt_decode from 'jwt-decode'
+import { AiOutlineHeart, AiOutlineHeartFill } from 'react-icons/ai'
+import { BsBookmark, BsBookmarksFill } from 'react-icons/bs'
 
 function PublicFeed() {
     const [profileImg, setProfileImg] = useState('')
@@ -14,17 +16,144 @@ function PublicFeed() {
             setProfileImg(userData.pfp.replace('pfp', ''))
         }
     })
+
+    const goToProfile = () => {
+        window.location.href = '/'
+    }
     return (
         <>
             {' '}
             <div className="public-feed">
                 <main>
                     <div className="feed-header">
-                        <h1>EXPLORE 🧭</h1>
+                        <h1>TABIBITO 🧭</h1>
                         <img
+                            onClick={goToProfile}
                             src={`http://localhost:3000/${profileImg}`}
                             alt=""
                         />
+                    </div>
+                    <div className="feed-content">
+                        <div className="feed-card">
+                            <div className="card-header">
+                                <img
+                                    style={{ width: '100%' }}
+                                    src={`http://localhost:3000/imageUpload-1653806035576.jpg`}
+                                />
+                            </div>
+                            <div className="feed-description">
+                                <div className="feed-description-header">
+                                    <div className="feed-description-header-user">
+                                        <img
+                                            src={`http://localhost:3000/${profileImg}`}
+                                            alt=""
+                                            width={'50px'}
+                                        />
+                                        <div className="feed-description-header-text">
+                                            <h3>Kobekoblanca</h3>
+                                            <h5>Sept. 5, 2022</h5>
+                                        </div>
+                                    </div>
+                                    <div className="feed-description-header-buttons">
+                                        <AiOutlineHeart
+                                            color="tomato"
+                                            size={30}
+                                        />
+                                        <BsBookmark color="gold" size={25} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="feed-card">
+                            <div className="card-header">
+                                <img
+                                    style={{ width: '100%' }}
+                                    src={`http://localhost:3000/imageUpload-1653806035576.jpg`}
+                                />
+                            </div>
+                            <div className="feed-description">
+                                <div className="feed-description-header">
+                                    <div className="feed-description-header-user">
+                                        <img
+                                            src={`http://localhost:3000/${profileImg}`}
+                                            alt=""
+                                            width={'50px'}
+                                        />
+                                        <div className="feed-description-header-text">
+                                            <h3>Kobekoblanca</h3>
+                                            <h5>Sept. 5, 2022</h5>
+                                        </div>
+                                    </div>
+                                    <div className="feed-description-header-buttons">
+                                        <AiOutlineHeart
+                                            color="tomato"
+                                            size={30}
+                                        />
+                                        <BsBookmark color="gold" size={25} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="feed-card">
+                            <div className="card-header">
+                                <img
+                                    style={{ width: '100%' }}
+                                    src={`http://localhost:3000/imageUpload-1653806035576.jpg`}
+                                />
+                            </div>
+                            <div className="feed-description">
+                                <div className="feed-description-header">
+                                    <div className="feed-description-header-user">
+                                        <img
+                                            src={`http://localhost:3000/${profileImg}`}
+                                            alt=""
+                                            width={'50px'}
+                                        />
+                                        <div className="feed-description-header-text">
+                                            <h3>Kobekoblanca</h3>
+                                            <h5>Sept. 5, 2022</h5>
+                                        </div>
+                                    </div>
+                                    <div className="feed-description-header-buttons">
+                                        <AiOutlineHeart
+                                            color="tomato"
+                                            size={30}
+                                        />
+                                        <BsBookmark color="gold" size={25} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="feed-card">
+                            <div className="card-header">
+                                <img
+                                    style={{ width: '100%' }}
+                                    src={`http://localhost:3000/imageUpload-1653806035576.jpg`}
+                                />
+                            </div>
+                            <div className="feed-description">
+                                <div className="feed-description-header">
+                                    <div className="feed-description-header-user">
+                                        <img
+                                            src={`http://localhost:3000/${profileImg}`}
+                                            alt=""
+                                            width={'50px'}
+                                        />
+                                        <div className="feed-description-header-text">
+                                            <h3>Kobekoblanca</h3>
+                                            <h5>Sept. 5, 2022</h5>
+                                        </div>
+                                    </div>
+                                    <div className="feed-description-header-buttons">
+                                        <AiOutlineHeart
+                                            color="tomato"
+                                            size={30}
+                                        />
+                                        <BsBookmark color="gold" size={25} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </main>
             </div>
