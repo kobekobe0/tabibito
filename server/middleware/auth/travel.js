ebca3877dc11d2d3aa6537a7f49e97ad54de7fa8
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-06-13 21:30:00 +0800

    accept image/gif

--- server/middleware/auth/travel.js
+++ server/middleware/auth/travel.js
@@ -15,7 +15,12 @@ const storage = multer.diskStorage({
 })
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    if (
+        file.mimetype === 'image/jpeg' ||
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/gif'
+    ) {
         cb(null, true)
     } else {
         cb(null, false)
