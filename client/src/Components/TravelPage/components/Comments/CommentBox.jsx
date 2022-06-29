510a126d6635e20d6e2d920114c4bad7149049cf
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-06-29 21:47:41 +0800

    sending comment request

--- client/src/Components/TravelPage/components/Comments/CommentBox.jsx
+++ client/src/Components/TravelPage/components/Comments/CommentBox.jsx
@@ -1,6 +1,33 @@
 import React from 'react'
+import axios from 'axios'
+import jwt_decode from 'jwt-decode'
+import { useState } from 'react'
 
-function CommentBox({ pfp }) {
+function CommentBox({ pfp, id, setComments, comments }) {
+    const [comment, setComment] = useState('')
+
+    const handlePost = (e) => {
+        e.preventDefault()
+        const userId = jwt_decode(localStorage.getItem('user')).id
+        console.log('clicked')
+        if (comment !== '') {
+            return axios
+                .post('/comment', {
+                    userId: userId,
+                    postId: id,
+                    comment: comment,
+                })
+                .then((res) => {
+                    console.log(res)
+                    setComments((prev) => [res.data, ...prev])
+                    setComment('')
+                })
+                .catch((err) => {
+                    alert(err)
+                })
+        }
+    }
+
     return (
         <div className="comment-form">
             <div className="comment-profile-img">
@@ -17,8 +44,15 @@ function CommentBox({ pfp }) {
                 />
             </div>
 
-            <input type="text" placeholder="Write a comment..." />
-            <button>Post</button>
+            <input
+                type="text"
+                placeholder="Write a comment..."
+                onChange={(e) => setComment(e.target.value)}
+                value={comment}
+            />
+            <button disabled={comment == ''} onClick={handlePost}>
+                Post
+            </button>
         </div>
     )
 }
