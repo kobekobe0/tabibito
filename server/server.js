6c9c68bced96efd24e373899d2f07aefacc38374
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-07-30 18:55:29 +0800

    try fix

--- server/server.js
+++ server/server.js
@@ -83,7 +83,7 @@ const { Server } = require('socket.io')
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3001' || 'https://tabibi-to.herokuapp.com/', //placeholder
+        origin: 'https://tabibi-to.herokuapp.com', //placeholder
         methods: 'GET, POST, PUT, DELETE, OPTIONS',
     },
 })
